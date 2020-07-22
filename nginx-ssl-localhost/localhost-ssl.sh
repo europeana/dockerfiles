c81e1101245996b6d1d8a9e8fc1c712b359f4c74
Richard Doe <richard.doe@rwdit.net>
2020-07-22 15:26:22 +0100

    Make directory during image build

--- nginx-ssl-localhost/localhost-ssl.sh
+++ nginx-ssl-localhost/localhost-ssl.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-mkdir /etc/nginx/ssl
-
 openssl req -x509 -newkey rsa:2048 -nodes \
   -keyout /etc/nginx/ssl/server.key -out /etc/nginx/ssl/server.crt \
   -days 365 -subj "/CN=localhost"
